159734940da09b612ae1ac7297b4cc727ed70cde
potatoThanh <thanhnguyen.cse@gmail.com>
2023-02-08 15:20:04 +1100

    test

--- docker/run.sh
+++ docker/run.sh
@@ -2,6 +2,10 @@
 source env.sh
 mkdir keycloak-db
 mkdir data
+sudo chmod -R +777 keycloak-db  
+sudo chmod -R +777 data  
 docker-compose pull
 docker-compose down
 docker-compose up -d --remove-orphans
+sudo chmod -R +777 keycloak-db  
+sudo chmod -R +777 data 
